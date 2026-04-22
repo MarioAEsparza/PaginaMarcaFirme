932c4336abd29e92792b6e91661e7944a24294d2
MarioAEsparza <jdaiosdhawdnoka@gmail.com>
2026-04-22 12:41:03 -0500

    version1

--- components/hero.tsx
+++ components/hero.tsx
@@ -79,11 +79,22 @@ export function Hero() {
       </div>
 
       {/* Indicador de scroll */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
-          <div className="w-1 h-3 bg-white/70 rounded-full" />
-        </div>
-      </div>
+  <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 animate-bounce">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-8 h-8 text-white/70"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19 9l-7 7-7-7"
+    />
+  </svg>
+  </div>
     </section>
   )
 }
